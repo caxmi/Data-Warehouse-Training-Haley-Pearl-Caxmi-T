fc9383de3b1b3e6fdb514b8cad530f74fcaf856d
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-05-27 11:24:22 +0530

    Delete May 27/Haley Pearl Caxmi T-Python Assignment directory

--- May 27/Haley Pearl Caxmi T-Python Assignment/1)Digit Sum Calculator.py
+++
@@ -1,3 +0,0 @@
-num = input("Enter a number: ")
-digit_sum = sum(int(digit) for digit in num)
-print(f"Sum of digits: {digit_sum}")
--- May 27/Haley Pearl Caxmi T-Python Assignment/8)Bill Splitter with Tip.py
+++
@@ -1,8 +0,0 @@
-bill = float(input("Enter total bill amount: "))
-people = int(input("Number of people: "))
-tip_percent = float(input("Tip percentage: "))
-
-total = bill + (bill * tip_percent / 100)
-per_person = total / people
-
-print(f"Each person should pay: ₹{per_person:.2f}")
