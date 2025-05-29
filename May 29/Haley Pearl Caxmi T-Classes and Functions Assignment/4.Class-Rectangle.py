62a9ae86935de6d12a077dd4b0d685dfc861661c
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-05-29 16:13:44 +0530

    Update 4.Class-Rectangle.py

--- May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/4.Class-Rectangle.py
+++ May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/4.Class-Rectangle.py
@@ -11,3 +11,10 @@ class Rectangle:
 
     def is_square(self):
         return self.length == self.width
+
+# Example
+r1 = Rectangle(5, 10)
+r2 = Rectangle(7, 7)
+print("Rectangle 1 area:", r1.area())
+print("Rectangle 2 area:", r2.is_square())
+print()
