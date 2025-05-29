812af668e2dd46ccbb4cff0f1baed145d8d80919
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-05-29 16:15:22 +0530

    Update 2.Temperature Converter.py

--- May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/2.Temperature Converter.py
+++ May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/2.Temperature Converter.py
@@ -1,11 +1,13 @@
 def convert_temp(value, unit):
     if unit.lower() == "c":
-        return (value * 9/5) + 32  # Celsius to Fahrenheit
+        return (value * 9/5) + 32
     elif unit.lower() == "f":
-        return (value - 32) * 5/9  # Fahrenheit to Celsius
+        return (value - 32) * 5/9
     else:
         return "Invalid unit"
-# Example Usage
-print("37°C in Fahrenheit:", convert_temp(37, 'C'))
-print("98.6°F in Celsius:", convert_temp(98.6, 'F'))
+
+val = float(input("Enter temperature value: "))
+unit = input("Enter unit (C/F): ")
+result = convert_temp(val, unit)
+print("Converted temperature:", result)
 print()
