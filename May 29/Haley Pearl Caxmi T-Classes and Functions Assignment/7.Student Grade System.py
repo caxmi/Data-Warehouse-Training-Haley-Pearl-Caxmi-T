a4a8c582c68d788fa163c5d97df9bb258d4fa484
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-05-29 16:16:59 +0530

    Update 7.Student Grade System.py

--- May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/7.Student Grade System.py
+++ May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/7.Student Grade System.py
@@ -9,10 +9,21 @@ class Student:
     def grade(self):
         avg = self.average()
         if avg >= 90:
-            return "A"
+            return 'A'
         elif avg >= 75:
-            return "B"
+            return 'B'
         elif avg >= 50:
-            return "C"
+            return 'C'
         else:
-            return "F"
+            return 'F'
+
+sname = input("Enter student name: ")
+n = int(input("Enter number of subjects: "))
+marks = []
+for i in range(n):
+    marks.append(float(input(f"Enter mark {i+1}: ")))
+
+stu = Student(sname, marks)
+print(f"{stu.name}'s Average: {stu.average():.2f}")
+print(f"{stu.name}'s Grade: {stu.grade()}")
+print()
