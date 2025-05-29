60abec3383d54c0fb7841858d1de6fc6edc7fe1d
Haley Pearl Caxmi T <129677772+caxmi@users.noreply.github.com>
2025-05-29 16:16:15 +0530

    Update 5.Class-BankAccount.py

--- May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/5.Class-BankAccount.py
+++ May 29/Haley Pearl Caxmi T-Classes and Functions Assignment/5.Class-BankAccount.py
@@ -5,12 +5,21 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
+        print("Deposited:", amount)
 
     def withdraw(self, amount):
         if amount > self.balance:
-            print("Insufficient balance")
+            print("Insufficient balance.")
         else:
             self.balance -= amount
+            print("Withdrawn:", amount)
 
     def get_balance(self):
         return self.balance
+
+acc_name = input("Enter account holder name: ")
+acc = BankAccount(acc_name, float(input("Enter initial balance: ")))
+acc.deposit(float(input("Enter deposit amount: ")))
+acc.withdraw(float(input("Enter withdrawal amount: ")))
+print("Current balance:", acc.get_balance())
+print()
